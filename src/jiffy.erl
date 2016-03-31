1febce3ca86c5ca5d5a3618ed3d5f125bb99e4c5
Paul J. Davis <paul.joseph.davis@gmail.com>
2016-03-31 11:00:10 -0500

    Fix force_utf8 for object keys
    Previously if a key was malformed UTF-8 and the user specified the
    `force_utf8` option we would fail to try and encode a fixed up version
    of the object. This was due to missing a clause to catch the
    `invalid_object_member_key` exception. This adds the clause and a couple
    tests to ensure it works.

--- src/jiffy.erl
+++ src/jiffy.erl
@@ -89,6 +89,9 @@ encode(Data, Options) ->
         {error, {invalid_string, _}} when ForceUTF8 == true ->
             FixedData = jiffy_utf8:fix(Data),
             encode(FixedData, Options -- [force_utf8]);
+        {error, {invalid_object_member_key, _}} when ForceUTF8 == true ->
+            FixedData = jiffy_utf8:fix(Data),
+            encode(FixedData, Options -- [force_utf8]);
         {error, _} = Error ->
             throw(Error);
         {partial, IOData} ->
@@ -189,6 +192,9 @@ encode_loop(Data, Options, Encoder, Stack, IOBuf) ->
     ForceUTF8 = lists:member(force_utf8, Options),
     case nif_encode_iter(Encoder, Stack, IOBuf) of
         {error, {invalid_string, _}} when ForceUTF8 == true ->
+            FixedData = jiffy_utf8:fix(Data),
+            encode(FixedData, Options -- [force_utf8]);
+        {error, {invalid_object_member_key, _}} when ForceUTF8 == true ->
             FixedData = jiffy_utf8:fix(Data),
             encode(FixedData, Options -- [force_utf8]);
         {error, _} = Error ->
